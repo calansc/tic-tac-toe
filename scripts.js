e916a3d2780e2f888bc54930af59d2c895977b5d
Chad Schroeder <77646640+calansc@users.noreply.github.com>
2023-02-24 08:03:24 -0500

    Add win conditions and win display text

--- scripts.js
+++ scripts.js
@@ -38,9 +38,9 @@ const displayController = (() => {
     // console.log(this.id);
     if (gameBoard.array[this.id] === "A") {
       gameBoard.updateArray(this.id, gameFlow.turner());
-      console.log(this.id, gameBoard.array);
+      // console.log(this.id, gameBoard.array);
       makeMove(this.id);
-      // gameFlow.turner();
+      gameFlow.winChecker();
     } else {
     }
   }
@@ -49,16 +49,70 @@ const displayController = (() => {
 
 const gameFlow = (() => {
   let turn = "O";
+  const playerTurn = document.querySelector(".turn");
   const turner = () => {
     if (turn === "X") {
       turn = "0";
+      playerTurn.textContent = "X - Player 1's Turn";
       return (turn = "O");
     } else {
       turn = "X";
+      playerTurn.textContent = "O - Player 2's Turn";
       return (turn = "X");
     }
   };
-  return { turn, turner };
+  const winChecker = () => {
+    if (
+      gameBoard.array[0] != "A" &&
+      gameBoard.array[0] === gameBoard.array[1] &&
+      gameBoard.array[0] === gameBoard.array[2]
+    ) {
+      playerTurn.textContent = "You win!";
+    } else if (
+      gameBoard.array[3] != "A" &&
+      gameBoard.array[3] === gameBoard.array[4] &&
+      gameBoard.array[3] === gameBoard.array[5]
+    ) {
+      playerTurn.textContent = "You win!";
+    } else if (
+      gameBoard.array[6] != "A" &&
+      gameBoard.array[6] === gameBoard.array[7] &&
+      gameBoard.array[6] === gameBoard.array[8]
+    ) {
+      playerTurn.textContent = "You win!";
+    } else if (
+      gameBoard.array[0] != "A" &&
+      gameBoard.array[0] === gameBoard.array[3] &&
+      gameBoard.array[0] === gameBoard.array[6]
+    ) {
+      playerTurn.textContent = "You win!";
+    } else if (
+      gameBoard.array[1] != "A" &&
+      gameBoard.array[1] === gameBoard.array[4] &&
+      gameBoard.array[1] === gameBoard.array[7]
+    ) {
+      playerTurn.textContent = "You win!";
+    } else if (
+      gameBoard.array[2] != "A" &&
+      gameBoard.array[2] === gameBoard.array[5] &&
+      gameBoard.array[2] === gameBoard.array[8]
+    ) {
+      playerTurn.textContent = "You win!";
+    } else if (
+      gameBoard.array[0] != "A" &&
+      gameBoard.array[0] === gameBoard.array[4] &&
+      gameBoard.array[0] === gameBoard.array[8]
+    ) {
+      playerTurn.textContent = "You win!";
+    } else if (
+      gameBoard.array[2] != "A" &&
+      gameBoard.array[2] === gameBoard.array[4] &&
+      gameBoard.array[2] === gameBoard.array[6]
+    ) {
+      playerTurn.textContent = "You win!";
+    }
+  };
+  return { turner, winChecker };
 })();
 
 displayController.populateBoard();
